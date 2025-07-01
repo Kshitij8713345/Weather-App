4a157b3fa4f8facd0a10db0eca601f2b58e8a871
kshitij soni <kshitijsonibca@gmail.com>
2025-07-01 23:14:46 +0530

    image changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,20 @@ import InfoBox from "./InfoBox";
 import SearchBox from "./SearchBox";
 import WeatherApp from "./WeatherApp";
 import './App.css';
+import bg from './assets/img1.webp'; // ✅ background image import
+
+const appStyle = {
+  backgroundImage: `url(${bg})`,
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  minHeight: '100vh',
+};
 
 function App() {
   return (
-    <>
-      < WeatherApp />
-    </>
+    <div style={appStyle}>
+      <WeatherApp />
+    </div>
   );
 }
 
